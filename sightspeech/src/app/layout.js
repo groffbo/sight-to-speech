5732264798f01183c8c17009b4a9da24cb85ead4
Natalia Cano <140561361+ncano1025@users.noreply.github.com>
2025-09-28 05:21:23 -0400

    layout update

--- sightspeech/src/app/layout.js
+++ sightspeech/src/app/layout.js
@@ -26,9 +26,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Moirai+One&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <TTSProvider>
             {children}
         </TTSProvider>
